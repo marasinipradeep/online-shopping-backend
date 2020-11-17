d4f02b1806fbb59b76982198664e045211a8bd53
marasinipradeep <marasinipradeep@gmail.com>
2020-11-17 17:25:40 +1030

    Graphql yoga server added on the top of prisma

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,20 @@
 // let's go!
+
+require('dotenv').config({path:'variable.env'});
+
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+//ToDo use express middleware to handle cookies(JWT)
+//ToDO use express middleware to populate current user
+
+server.start({
+    cors:{
+        credentials:true,
+        origin:process.env.FRONTEND_URL
+    },
+},deets=>{
+    console.log(`server is running on port http://localhost:${deets.port}`)
+});
