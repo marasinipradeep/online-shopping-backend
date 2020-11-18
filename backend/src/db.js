c984e23f4b450ea34b517ff25f84bc0e0294ff1f
marasinipradeep <marasinipradeep@gmail.com>
2020-11-18 18:30:55 +1030

    prisma.graphql file generated successfully using prisma.yml script

--- backend/src/db.js
+++ backend/src/db.js
@@ -3,7 +3,7 @@
 const {Prisma} =require('prisma-binding');
 const db = new Prisma({
     typeDefs:'src/generated/prisma.graphql',
-    endpoint:process.env.PRISMA_ENDPOINT,
+    endpoint:"https://us1.prisma.sh/pradeep-marasini/e-store/dev",//process.env.PRISMA_ENDPOINT,
     secret:process.env.PRISMA_SECRET,
     debug:true
 })
--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,17 @@
-const Mutations = {};
+const Mutations = {
+
+    async createItem(parent,args,ctx,info){
+        //ToDo check if they logged in
+        console.log("inside  create item Mutation")
+        const item =await ctx.db.mutation.createItem({
+            data:{
+                ...args
+            }
+        },info);
+        console.log("before return create item")
+        console.log(item)
+        return item;
+    }
+};
 
 module.exports = Mutations;
