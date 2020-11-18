a1b23312c9f2c95ea11605ac45f9a7b40cfff375
marasinipradeep <marasinipradeep@gmail.com>
2020-11-18 19:01:46 +1030

    prisma and graphql setup on backend completed

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,8 +1,13 @@
+ const {forwardTo} =require('prisma-binding')
+ 
  const Query = {
-    async  items(parent,args,ctx,info){
-        const items = await ctx.db.query.item();
-        return items;
-   }
+
+   items:forwardTo('db')
+
+   //  async  items(parent,args,ctx,info){
+   //      const items = await ctx.db.query.items();
+   //      return items;
+   // }
 };
 
 module.exports = Query;
