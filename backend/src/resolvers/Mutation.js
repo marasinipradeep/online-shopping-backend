457cb4fbda792e97fe9c52dac6d0da4b2a244764
marasinipradeep <marasinipradeep@gmail.com>
2020-11-21 12:31:15 +1030

    delete Mutation and schema completed

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -25,7 +25,20 @@ const Mutations = {
             },
         },info
         );
-    }
+    },
+
+   async deleteItem (parent,args,ctx,info){
+        console.log("inside deleteItem")
+        console.log(args.id)
+
+        const where = {id:args.id}
+        //1.find the item
+
+        const item = await ctx.db.query.item({where},`{id title}`)
+        //2. check if they own that item,or have the permissions
+        //3. Delete it
+        return ctx.db.mutation.deleteItem({where},info);
+    },
 };
 
 module.exports = Mutations;
