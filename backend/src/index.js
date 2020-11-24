8dbd4e5f0cdedb8a67814475b6555073591aca93
marasinipradeep <marasinipradeep@gmail.com>
2020-11-24 12:23:53 +1030

    Schema to create Jwt token and checking user logged in completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@
 require('dotenv').config({path:'variables.env'});
 
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken')
 const createServer = require('./createServer');
 const db = require('./db');
 
@@ -12,6 +13,18 @@ const server = createServer();
 
 //use express middleware to handle cookies(JWT)
 server.express.use(cookieParser());
+
+//decode the JWT so we can get the user Id on each request
+server.express.use((req,res,next)=>{
+    const {token} =req.cookies;
+    console.log(`the token is ${token}`)
+    if(token){
+        const {userId}=jwt.verify(token,process.env.APP_SECRET);
+        //put the userId onto the req for future resquests to access
+        req.userId = userId;
+    }
+    next();
+});
 
 //ToDO use express middleware to populate current user
 
