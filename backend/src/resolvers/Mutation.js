addfb14beecc640599380fcf615d151fea41b033
marasinipradeep <marasinipradeep@gmail.com>
2020-12-20 18:36:22 +1030

    #Data relationship between user and added item completed.If user not logged in thrown error

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -9,10 +9,21 @@ const Mutations = {
 
     async createItem(parent, args, ctx, info) {
         //ToDo check if they logged in
-        console.log("inside createItem")
-        console.log(args)
+
+        if(!ctx.request.userId){
+            throw new Error('You must be logged in to do that!');
+        }
+
         const item = await ctx.db.mutation.createItem({
             data: {
+
+                //This is how to create a relationship between an item and user
+                user:{
+                    connect:{
+                        id:ctx.request.userId
+                    }
+
+                },
                 ...args
             }
         }, info);
